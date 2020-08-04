93625f616aa87510f9a5369467f311c5de836ca6
tugzera <damascenobdm@gmail.com>
2020-08-04 18:50:24 -0300

    #fix timestamp

--- database/migrations/1596505482950_service_order_schema.js
+++ database/migrations/1596505482950_service_order_schema.js
@@ -7,20 +7,18 @@ class ServiceOrderSchema extends Schema {
   up() {
     this.create('service_orders', (table) => {
       table.increments()
-      table.date('opened_at').notNullable()
+      table.timestamp('opened_at').notNullable()
       table.integer('client_id').unsigned().index()
       table
         .foreign('client_id')
         .references('id')
         .on('clients')
-        .onUpdate('cascade')
         .onDelete('cascade')
       table.integer('collaborator_id').unsigned().index()
       table
         .foreign('collaborator_id')
         .references('id')
         .on('collaborators')
-        .onUpdate('cascade')
         .onDelete('cascade')
       table.decimal('latitude').notNullable()
       table.decimal('longitude').notNullable()
